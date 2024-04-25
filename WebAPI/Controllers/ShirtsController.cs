078e5f394495b843d71cc829afd2862380ee5437
chris.kuo <126221813+beyondcars@users.noreply.github.com>
2024-04-25 15:20:27 +0800

    init ShirtsController

---
+++ WebAPI/Controllers/ShirtsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShirtsController: ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetShirts()
+        {
+            return Ok(123);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetShirtById(int id)
+        {
+            return Ok($"{id}");
+        }
+
+        [HttpPost]
+        public IActionResult CreateShirt(Shirt shirt)
+        {
+            return Ok(shirt);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateShirt(int id, Shirt shirt)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteShirt(int id) 
+        {
+            return Ok(id);
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -8,27 +9,6 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+app.MapControllers();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
