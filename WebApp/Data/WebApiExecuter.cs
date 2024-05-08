f8d4d2d154a05c087f54ce457b7ddac5425604b6
chris.kuo <126221813+beyondcars@users.noreply.github.com>
2024-05-08 16:12:53 +0800

    AddJwtToHeader

--- WebApp/Data/WebApiExecuter.cs
+++ WebApp/Data/WebApiExecuter.cs
@@ -1,19 +1,26 @@
-﻿namespace WebApp.Data
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace WebApp.Data
 {
     public class WebApiExecuter : IWebApiExecuter
     {
         private const string apiName = "ShirtsApi";
+        private const string authApiName = "AuthorityApi";
 
         private readonly IHttpClientFactory httpClientFactory;
+        private readonly IConfiguration configuration;
 
-        public WebApiExecuter(IHttpClientFactory httpClientFactory)
+        public WebApiExecuter(IHttpClientFactory httpClientFactory, IConfiguration configuration)
         {
             this.httpClientFactory = httpClientFactory;
+            this.configuration = configuration;
         }
 
         public async Task<T?> InvokeGet<T>(string url)
         {
             var httpClient = httpClientFactory.CreateClient(apiName);
+            await AddJwtToHeader(httpClient);
             //return await httpClient.GetFromJsonAsync<T>(url);
 
             var request = new HttpRequestMessage(HttpMethod.Get, url);
@@ -26,6 +33,8 @@
         public async Task<T?> InvokePost<T>(string url, T obj)
         {
             var httpClient = httpClientFactory.CreateClient(apiName);
+            await AddJwtToHeader(httpClient);
+
             var response = await httpClient.PostAsJsonAsync<T>(url, obj);
             await HandlePotentialError(response);
 
@@ -35,6 +44,8 @@
         public async Task InvokePut<T>(string url, T obj)
         {
             var httpClient = httpClientFactory.CreateClient(apiName);
+            await AddJwtToHeader(httpClient);
+
             var response = await httpClient.PutAsJsonAsync(url, obj);
             await HandlePotentialError(response);
         }
@@ -42,6 +53,8 @@
         public async Task InvokeDelete(string url)
         {
             var httpClient = httpClientFactory.CreateClient(apiName);
+            await AddJwtToHeader(httpClient);
+
             var response = await httpClient.DeleteAsync(url);
             await HandlePotentialError(response);
         }
@@ -53,6 +66,29 @@
                 var errorJson = await response.Content.ReadAsStringAsync();
                 throw new WebApiException(errorJson);
             }
+        }
+
+        private async Task AddJwtToHeader(HttpClient httpClient)
+        {
+            var clientId = configuration.GetValue<string>("ClientId");
+            var secret = configuration.GetValue<string>("Secret");
+
+            // Authenticate
+            var authClient = httpClientFactory.CreateClient(authApiName);
+            var response = await authClient.PostAsJsonAsync("auth", new AppCredential
+            {
+                ClientId = clientId,
+                Secret = secret
+            });
+
+            response.EnsureSuccessStatusCode();
+
+            // get the jwt
+            string strToken = await response.Content.ReadAsStringAsync();
+            var token = JsonConvert.DeserializeObject<JwtToken>(strToken);
+
+            // pass the jwt to endpoints through the http headers
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token?.AccessToken);
         }
     }
 }
